ed06d22629fc26accfb4b1d68b59b24026cbe14b
Sureya Pragaash G <sureyapragaash09@gmail.com>
2019-01-12 12:32:59 +0530

    solution No 4 Modified

--- Find_More_than_once.py
+++ Find_More_than_once.py
@@ -2,15 +2,30 @@
 #input = array 
 #output = if more than one element is found return true else false
 
+def count(a):
+    counti = 0
+    while(a[counti:]):
+        counti += 1
+
+    return counti
+
 def find_more_than_once(array):
     flag = False
-    for i in array:
+    i = 0
+    b = count(array)
+    while (i!=b):
+        start = array[i]
         j = i + 1
-        for j in array:
-            if i == j:
+        while (j!=b):
+            if (start == array[j]):
                 flag = True
                 break
+            j += 1
+        i+=1
     return flag
 
-array = [7,9,4,2,2]
+        
+    
+
+array = [7,9,1,2,4,56,3,34,23,45,56,20,41,22,3]
 print(find_more_than_once(array))
